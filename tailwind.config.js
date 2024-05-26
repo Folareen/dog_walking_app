3095662dae6d00376b5edc70c67691e60e67e135
Folareen <sakawahab03@gmail.com>
2024-05-26 22:45:52 +0100

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      'opensans': ['opensans'],
+      'opensans_bold': ['opensans_bold'],
+      'roboto': ['roboto'],
+      'roboto_bold': ['roboto_bold'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
