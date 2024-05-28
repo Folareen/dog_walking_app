361dfbc5bd74683e2fcd970b0dcc2c7789af6c0c
Folareen <sakawahab03@gmail.com>
2024-05-28 17:08:00 +0100

    done with login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,10 +1,9 @@
-import { View, Text, ImageBackground, TextInput, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react'
+import { ImageBackground, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { supabase } from '../supabase'
 import toast from '../utils/toast'
-import useAuthStore from '../stores/useAuthStore'
 
 
 const Login = () => {
@@ -14,8 +13,6 @@ const Login = () => {
     const [submitting, setSubmitting] = useState(false)
 
     const { navigate } = useNavigation()
-
-    const { user, setUser } = useAuthStore()
 
     const handleLogin = async () => {
         try {
@@ -43,10 +40,10 @@ const Login = () => {
                 <View className='w-4/5 '>
                     <Text className='text-white text-4xl mb-6 font-opensans_bold text-center'>Welcome Back!</Text>
 
-                    <TextInput placeholder='Email Address' className='px-4 py-2.5 bg-white rounded-lg font-semibold mt-4' value={email} onChangeText={(text) => {
+                    <TextInput placeholder='Email Address' className='px-4 py-2.5 bg-white rounded-lg font-roboto mt-4' value={email} onChangeText={(text) => {
                         setEmail(text)
                     }} />
-                    <TextInput placeholder='Password' className='px-4 py-2.5 bg-white rounded-lg font-semibold mt-4' secureTextEntry={true} value={password} onChangeText={(text) => {
+                    <TextInput placeholder='Password' className='px-4 py-2.5 bg-white rounded-lg font-roboto mt-4' secureTextEntry={true} value={password} onChangeText={(text) => {
                         setPassword(text)
                     }} />
 
